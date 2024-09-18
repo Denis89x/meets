28c3e90e2f9adbffcde1d87fa5c4df923b6c2622
Lebenkov Denis <sabzz1ro3@mail.ru>
2024-09-18 21:30:08 +0300

    Added jwt filter

--- src/main/java/dev/lebenkov/meets/api/security/JwtUtilService.java
+++ src/main/java/dev/lebenkov/meets/api/security/JwtUtilService.java
@@ -5,8 +5,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -14,6 +17,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class JwtUtilService {
 
     @Value("${application.security.jwt.secret-key}")
