fe1b341bd7cd537ec4a50dbb7bfa3c4a697e63fe
Lebenkov Denis <sabzz1ro3@mail.ru>
2024-09-18 21:19:49 +0300

    Added token repository

---
+++ src/main/java/dev/lebenkov/meets/storage/repository/TokenRepository.java
@@ -0,0 +1,22 @@
+package dev.lebenkov.meets.storage.repository;
+
+import dev.lebenkov.meets.storage.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query(value = """
+            select t from Token t inner join User u\s
+            on t.user.userId = u.userId\s
+            where u.userId = :id and (t.expired = false or t.revoked = false)\s
+            """)
+    List<Token> findAllValidTokenByUser(Long id);
+
+    Optional<Token> findByToken(String token);
+}
