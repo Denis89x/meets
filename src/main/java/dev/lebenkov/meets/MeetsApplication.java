a0e44976565b5dec78401413e0893b1f9185f7e0
Lebenkov Denis <sabzz1ro3@mail.ru>
2024-09-18 20:55:16 +0300

    Project initialized

---
+++ src/main/java/dev/lebenkov/meets/MeetsApplication.java
@@ -0,0 +1,13 @@
+package dev.lebenkov.meets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lebenkov/meets/MeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lebenkov.meets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
